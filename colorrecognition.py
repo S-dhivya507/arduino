f8c31e1b60ebce90e057587e1cd4350bf894e7c3
Dhivya Shanmugavel <dhivyashanmugavel26@gmail.com>
2025-11-06 21:09:24 +0530

    Update colorrecognition.py

--- colorrecognition.py
+++ colorrecognition.py
@@ -1,53 +1,45 @@
-#define s0 8 
-#define s1 9
-#define s2 10
-#define s3 11
-#define out 12
+const int s0 = 8, s1 = 9, s2 = 10, s3 = 11, out = 12;
 
-int data = 10; 
-
-void setup() 
-{
-  pinMode(s0, OUTPUT); 
+void setup() {
+  pinMode(s0, OUTPUT);
   pinMode(s1, OUTPUT);
   pinMode(s2, OUTPUT);
   pinMode(s3, OUTPUT);
   pinMode(out, INPUT);
 
   Serial.begin(9600);
-
-  // Set frequency scaling to 100% (HIGH-HIGH)
-  digitalWrite(s0, HIGH); 
-  digitalWrite(s1, HIGH); 
+  digitalWrite(s0, HIGH);
+  digitalWrite(s1, HIGH);
+  Serial.println("Color Recognition Ready");
 }
 
 void loop() {
-  // RED filter (LOW-LOW)
-  digitalWrite(s2, LOW); 
+  digitalWrite(s2, LOW);
   digitalWrite(s3, LOW);
-  Serial.print("Red value= "); 
-  GetData(); 
+  Serial.print("Red = ");
+  readColor();
 
-  // BLUE filter (LOW-HIGH)
   digitalWrite(s2, LOW);
   digitalWrite(s3, HIGH);
-  Serial.print("Blue value= ");
-  GetData();
+  Serial.print("Blue = ");
+  readColor();
 
-  // GREEN filter (HIGH-HIGH)
   digitalWrite(s2, HIGH);
   digitalWrite(s3, HIGH);
-  Serial.print("Green value= ");
-  GetData();
+  Serial.print("Green = ");
+  readColor();
 
   Serial.println();
   delay(2000);
 }
 
-void GetData() {
-  data = pulseIn(out, LOW); // Measure duration of LOW pulse
-  Serial.print(data); 
-  Serial.print("\t"); 
-  delay(20);
+void readColor() {
+  long d = pulseIn(out, LOW, 30000);
+  if (d == 0)
+    Serial.print("NoSignal");
+  else
+    Serial.print(d);
+
+  Serial.print("\t");
+  delay(1000);
 }
- colour recognition
